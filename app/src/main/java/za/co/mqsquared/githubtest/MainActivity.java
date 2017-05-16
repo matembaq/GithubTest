5ecac4636ac45dbe86bfb465bf8e8c520ad39705
Matemba Qandana <MatembaQ@sita.co.za>
2017-05-16 06:04:43 +0200

    Initial commit

---
+++ app/src/main/java/za/co/mqsquared/githubtest/MainActivity.java
@@ -0,0 +1,13 @@
+package za.co.mqsquared.githubtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
